3f2e341ec5d28b395e974000a5f1cfc9cdf65bea
Rzesiasty <32109666+Rzesiasty@users.noreply.github.com>
2019-10-07 19:59:08 +0200

    commit script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'https://intense-springs-06026.herokuapp.com/v1/task';
-  const trelloApiRoot = 'https://intense-springs-06026.herokuapp.com/v1/trello';
+  const apiRoot = 'https://intense-springs-06026.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://intense-springs-06026.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
